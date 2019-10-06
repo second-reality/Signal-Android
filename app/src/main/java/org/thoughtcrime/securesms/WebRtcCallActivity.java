e5087ea51726c4251f5782d0ec6cc26374dd54e9
Pierrick Bouvier <bouvier.pierrick@yahoo.fr>
2019-10-06 19:40:06 +0200

    no delivery patch + no ringing phone

--- app/src/main/java/org/thoughtcrime/securesms/WebRtcCallActivity.java
+++ app/src/main/java/org/thoughtcrime/securesms/WebRtcCallActivity.java
@@ -244,7 +244,9 @@ public class WebRtcCallActivity extends Activity {
   }
 
   private void handleCallRinging(@NonNull WebRtcViewModel event) {
-    callScreen.setActiveCall(event.getRecipient(), getString(R.string.RedPhone_ringing), event.getLocalRenderer());
+    // Do not report if user is ringing
+    //callScreen.setActiveCall(event.getRecipient(), getString(R.string.RedPhone_ringing), event.getLocalRenderer());
+    callScreen.setActiveCall(event.getRecipient(), getString(R.string.RedPhone_dialing), event.getLocalRenderer());
   }
 
   private void handleCallBusy(@NonNull WebRtcViewModel event) {
